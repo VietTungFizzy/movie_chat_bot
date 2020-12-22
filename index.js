9b8505dcbf598a91a8f17f25f47cd085c97b5a95
Tung <31438811+VietTungFizzy@users.noreply.github.com>
2020-12-22 22:22:46 +0700

    Basic stuff

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+// will use this later to send requests
+const http = require('http')
+// import env variables
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.status(200).send('Server is working.')
+})
+
+app.get('/getmovie', (req, res) => {
+    const movieToSearch =
+		req.body && req.body.result && req.body.result.parameters && req.body.result.parameters.movie
+			? req.body.result.parameters.movie
+            : ''
+    const reqUrl = encodeURI(
+        `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${movieToSearch}`
+    )
+    http.get(
+        reqUrl,
+        responseFromAPI => {
+            let completeResponse = ''
+			responseFromAPI.on('data', chunk => {
+				completeResponse += chunk
+			})
+
+            responseFromAPI.on('end', () => {
+                const movie = JSON.parse(completeResponse)
+
+                let responseData = `${movie.Title} was released in the year ${movie.Year}. 
+                It is directed by ${movie.Director} and stars ${movie.Actors}.\n 
+                Here some glimpse of the plot: ${movie.Plot}.`
+
+                return res.json({
+                    fulfillmentText: responseData,
+					source: 'getmovie'
+                })
+            })
+        }
+    )
+})
+app.listen(port, () => {
+    console.log(`🌏 Server is running at http://localhost:${port}`)
+})
